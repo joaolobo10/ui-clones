2be9cddf91cd7c008d8b549321ca3e94d90da286
joaolobo10 <joaocarloslobo10@gmail.com>
2025-05-23 10:02:13 -0300

    adição de pasta

---
+++ store-fgr/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/sections/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-zinc-900">
+      <Header />
+    </main>
+  );
+}
